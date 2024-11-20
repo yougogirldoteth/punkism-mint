f3072b18e2ae32683986c7ab02af54bc8e9ae382
yougogirldoteth <linus@1001.digital>
2024-11-20 19:01:06 +0100

    new mintgaspricepopover component

--- components/MintGasPricePopover.client.vue
+++ components/MintGasPricePopover.client.vue
@@ -3,21 +3,18 @@
     <template #trigger><MintGasPriceStatusIndicator :mint-count="2" class="gas-button"/></template>
     <template #content>
       <div class="prose">
-        <h1>Mint Pricing</h1>
-        <p>Artifacts are priced based on the ethereum network fees at the time of collecting.</p>
-        <p>Network fees (Gas fees) are an essential component of of securing and running decentralized blockchains.</p>
-        <p>
-          The cost to store and secure the object on the network is mirrored as compensation to the artist,
-          creating a direct link between network value and creator reward.
-        </p>
+        <h1>{{ $t('popover.mint_pricing')}}</h1>
+        <p>{{ $t('popover.pricing_info')}}</p>
+        <p>{{ $t('popover.network_fees')}}</p>
+        <p>{{ $t('popover.artist_reward') }}</p>
         <table>
           <tbody>
             <tr>
-              <td>Gas price</td>
+              <td>{{ $t('popover.gas_price') }}</td>
               <td><GasPrice /></td>
             </tr>
             <tr>
-              <td>Cost to mint</td>
+              <td>{{ $t('popover.cost_to_mint') }}</td>
               <td>
                 <span>
                   60.000 gas * <GasPrice />
@@ -25,16 +22,52 @@
               </td>
             </tr>
             <tr>
-              <td>Ethereum fee (50%)</td>
+              <td>{{ $t('popover.ethereum_fee') }}</td>
               <td><MintGasPrice /></td>
             </tr>
             <tr>
-              <td>Artist <abbr title="Artist Compensation">comp.</abbr> (50%)</td>
+              <td>{{ $t('popover.artist_comp') }} (50%)</td>
               <td><MintGasPrice /></td>
             </tr>
             <tr>
-              <td>Mint price</td>
+              <td>{{ $t('popover.mint_price') }}</td>
               <td><MintGasPrice :mint-count="2" /></td>
+            </tr>
+          </tbody>
+        </table>
+        <p>{{ $t('popover.mint_more_info') }}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>{{ $t('popover.amount') }}</th>
+              <th>{{ $t('popover.eth_fee')}}</th>
+              <th>{{  $t('popover.artist_comp') }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="number"
+                  min="1"
+                  :value="mintAmount"
+                  @input="amount = $event.target.value"
+                  @focus="animate = false"
+                  :class="{ animate }"
+                />
+              </td>
+              <td>
+                {{ ethPercentage }}%
+                <MintGasPrice v-slot="{ dollarPrice }" :mint-count="1">
+                  <span>(${{ dollarPrice }})</span>
+                </MintGasPrice>
+              </td>
+              <td>
+                {{ artistPercentage }}%
+                <MintGasPrice v-slot="{ dollarPrice }" :mint-count="parseInt(amount)">
+                  <span>(${{ dollarPrice }})</span>
+                </MintGasPrice>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -44,6 +77,26 @@
 </template>
 
 <script setup>
+const amount = ref('2')
+const mintAmount = computed(() => amount.value ? Math.max(1, parseInt(amount.value)) : 1)
+const ethPercentage = computed(() => (100 / Math.max(1, mintAmount.value + 1)).toFixed(2))
+const artistPercentage = computed(() => (100 - ethPercentage.value).toFixed(2))
+
+const animate = ref(true)
+const runAnimation = async () => {
+  await delay(3000)
+  if (! animate.value) return
+  const amounts = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+  const mints = takeRandom(amounts)
+
+  amount.value = mints
+
+  runAnimation()
+}
+
+onMounted(() => {
+  runAnimation()
+})
 </script>
 
 <style scoped>
@@ -58,22 +111,33 @@
           white-space: nowrap;
         }
       }
+
+      &:has(input) {
+        padding: 0;
+        width: 33%;
+      }
+
+      input {
+        border-radius: 0;
+        border: 0;
+      }
     }
 
-    :deep(.usd) {
-      display: inline-block;
-      margin-left: auto;
+    thead {
+      th {
+        text-align: left;
+        white-space: nowrap;
+        font-weight: var(--font-weight);
+      }
     }
-  }
-
-  .gas-button {
-    color: var(--gray-z-5);
-    background-color: transparent;
-    transition: color 0.3s ease;
 
     &:hover {
       color: var(--cp-pink);
       background-color: transparent;
     }
+  }
+
+  .gas-button:hover {
+    color: var(--cp-pink);
   }
 </style>
